830ad3314ccac3d9e0fef15037c1bca0687b6b70
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-07 11:46:52 +0330

    {Add - App} /Add redux

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { generalReducer } from './reducers/mainReducer/generalReducer';
+
+const rootReducer = combineReducers({
+  stateGeneral: generalReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : (f) => f
+  )
+);
+
+export default store;
