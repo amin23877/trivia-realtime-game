861036cbf6520e4fdb6b04f9e81810dad72187ac
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-07 15:37:09 +0330

    {Update - index & App} /Use of RouterConfig

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import { Box, Container, CssBaseline, Drawer, Grid, IconButton, List, ListItem, ListItemIcon, ListItemText, makeStyles } from "@material-ui/core";
-import PageRoutes from "./Router/PageRoutes";
+import {
+  Box,
+  Container,
+  CssBaseline,
+  Drawer,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+} from '@material-ui/core';
+import PageRoutes from './Router/PageRoutes';
 import './App.css';
-import { SideDrawer, Header, Login } from "./components";
+import { SideDrawer, Header, Login } from './components';
+import RouterConfig from 'common/services/Router';
 
+//
 
+const useStyles = makeStyles((theme) => ({
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary,
+  },
+  page: {
+    paddingTop: '3.8rem',
+  },
+}));
 
+export default function App() {
+  const classes = useStyles();
 
-
-// 
-
-
-
-const useStyles = makeStyles ( (theme ) => ({
-    link : {
-        textDecoration : 'none',
-        color: theme.palette.text.primary
-    },
-    page : {
-        paddingTop: '3.8rem'
-    }
-}))
-
-
-export default function App () {
-
-
-    const classes = useStyles();
-
-    return (
-        <div>
-            <Login/>
-            {/* <CssBaseline />
-            <header className="header">
-                <Header/>
-            </header>
-            <div style={{display : 'flex'}}>
-                <SideDrawer />
-                <Container className={classes.page} position="static">
-                    <PageRoutes />
-                </Container>
-            </div> */}
-        </div>
-    )
+  return <RouterConfig />;
 }
+
+// <div>
+//     <Login/>
+//     {/* <CssBaseline />
+//     <header className="header">
+//         <Header/>
+//     </header>
+//     <div style={{display : 'flex'}}>
+//         <SideDrawer />
+//         <Container className={classes.page} position="static">
+//             <PageRoutes />
+//         </Container>
+//     </div> */}
+// </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 import store from 'redux/store'; // #redux step1
 import App from './App';
 
 ReactDOM.render(
   // #redux step2
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
