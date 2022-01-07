9b4615453c87c97c646ffb10b2e9837d02ea0b16
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-07 16:58:54 +0330

    {Add - ApiCall} /Create /Add some functions

---
+++ src/common/values/CORE.js
@@ -0,0 +1,10 @@
+export const BASE_URL = 'https://...';
+
+export const CORE = {
+  ERROR_MESSAGE: {
+    default: 'D Error',
+    server: 'Server Error',
+    timeOut: 'TimeOut Error',
+    notJson: 'NotJson Error',
+  },
+};
