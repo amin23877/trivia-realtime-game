85b08465b46c49d65e8cd5b706351427612e237a
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-07 21:21:15 +0330

    {Update - RouterConfig} /Use of ProtectedRoute

--- src/common/services/Router.js
+++ src/common/services/Router.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
+import ProtectedRoute from './ProtectedRouter';
+
 import Home from 'pages/home/Home';
 import { Login } from 'components/Login/Login';
 import { ConfirmCode } from 'components/Login/ConfirmCode';
@@ -10,7 +12,10 @@ const RouterConfig = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route exact path='/' element={<ProtectedRoute />}>
+          <Route exact path='/' element={<Home />} />
+        </Route>
+
         <Route path='/login' element={<Login />} />
         <Route path='/otp' element={<ConfirmCode />} />
         <Route path='*' element={<Home />} />
