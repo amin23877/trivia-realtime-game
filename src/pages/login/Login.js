3fa69c27fb35e7c26c3eca67e68f08836e064de3
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-08 23:18:19 +0330

    {Add - Login} /Add some template & styles

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,27 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import ApiCall from 'common/services/ApiCall';
 
 import './Login.scss';
+import logo from 'assets/images/logo/logo.svg';
+import mainImg from 'assets/images/pics/login-login.svg';
+import { TextField } from '@material-ui/core';
 
 const Login = () => {
   const navigate = useNavigate();
 
   const apiCall = new ApiCall();
 
-  const handleGetOtp = () => {
-    localStorage.setItem('phone', 'state.formData.phone_number');
+  const [phone, setPhone] = useState('');
+  const [isValidPhone, setIsValidPhone] = useState(false);
+  const [messageError, setMessageError] = useState(
+    'Please enter a valid phone number'
+  );
 
-    apiCall
-      .post('user/register', { phone: 'state.formData.phone_number' })
-      .then((res) => {
-        console.log('res > ', res);
-        navigate('/otp');
-      })
-      .catch((err) => {
-        console.log('err > ', err);
-      });
+  const handleChangePhone = (e) => {
+    console.log(e.target.value);
+    setPhone(e.target.value);
+
+    const phoneno = /^0\d{10}$/;
+
+    // if (phone.match(phoneno) && phone.length > 10) {
+    //   setIsValidPhone(true);
+    // } else {
+    //   setIsValidPhone(true);
+    // }
+  };
+
+  const handleGetOtp = () => {
+    localStorage.setItem('phone', phone);
+
+    if (phone && isValidPhone) {
+      apiCall
+        .post('user/register', { phone })
+        .then((res) => {
+          console.log('res > ', res);
+          navigate('/otp');
+        })
+        .catch((err) => {
+          console.log('err > ', err);
+        });
+    } else {
+      // TODO
+    }
   };
 
   useEffect(() => {
@@ -34,8 +60,39 @@ const Login = () => {
   }, []);
 
   return (
-    <div className='w-100 h-100 login br2'>
-      <div className=''>Login</div>
+    <div className='w-100 h-100 p-3 d-flex flex-column align-items-center login'>
+      <img src={logo} />
+
+      <div className='login-body'>
+        <p className='title'>Log in</p>
+        <img src={mainImg} />
+
+        <form noValidate autoComplete='off' className='_dish-textField'>
+          <div className=''>
+            <p className='lable'>Phone Number</p>
+            <TextField
+              autoFocus={true}
+              type='tel'
+              placeholder='Enter your phone number'
+              className=''
+              helperText={phone && !isValidPhone ? messageError : ''}
+              variant='outlined'
+              inputProps={{ maxLength: 11 }}
+              // value={phone}
+              error={phone && !isValidPhone}
+              onKeyPress={(e) => handleChangePhone(e)}
+            />
+          </div>
+        </form>
+
+        <button
+          className='login-btn'
+          disabled={!isValidPhone}
+          onClick={handleGetOtp}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 };
