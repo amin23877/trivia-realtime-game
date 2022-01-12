6098bb9a7df749cd0ececb776fb83b5a41fe8dd4
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-12 08:08:54 +0330

    {Add - HomeTopics} /Create

---
+++ src/pages/home/homeComponents/homeTopics/HomeTopics.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './HomeTopics.scss';
+
+const HomeTopics = () => {
+  return (
+    <div className='w-100 h-100 home-topics'>
+      <div className='home-topics-body'></div>
+    </div>
+  );
+};
+
+export default HomeTopics;
