917fc21252850dd3a98b4910ab88b44ef9552554
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-14 23:07:59 +0330

    {Add - Notification} /Create

---
+++ src/pages/menuPages/notification/Notification.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './Notification.scss';
+
+const Notification = () => {
+  return <div className='w-100 h-100 notification bg1'>Notification</div>;
+};
+export default Notification;
