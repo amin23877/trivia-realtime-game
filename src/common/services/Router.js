2445c95784fdb3a1509934e44126c361e2dfd6e4
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-14 23:09:34 +0330

    {Add - RouterConfig} /Add routes

--- src/common/services/Router.js
+++ src/common/services/Router.js
@@ -18,6 +18,10 @@ import VerificationCode from 'pages/login/VerificationCode';
 import Profile from 'pages/profile/Profile';
 import HomeTopicsInner from 'pages/home/homeComponents/homeTopics/HomeTopicsInner';
 import QuickPlay from 'pages/quickPlay/QuickPlay';
+import ProfileEdit from 'pages/profile/ProfileEdit';
+import Wallet from 'pages/menuPages/wallet/Wallet';
+import Notification from 'pages/menuPages/notification/Notification';
+import SearchExplore from 'pages/searchExplore/SearchExplore';
 
 const RouterConfig = () => {
   return (
@@ -25,30 +29,34 @@ const RouterConfig = () => {
       <Routes>
         <Route exact path='/' element={<ProtectedRoute />}>
           <Route exact path='/' element={<Home />} />
-        </Route>
-
-        <Route exact path='/topics' element={<ProtectedRoute />}>
           <Route exact path='/topics/:id' element={<HomeTopicsInner />} />
-        </Route>
-
-        <Route exact path='/menu' element={<ProtectedRoute />}>
-          <Route exact path='/menu' element={<Menu />} />
-        </Route>
-
-        <Route exact path='/friends' element={<ProtectedRoute />}>
-          <Route exact path='/friends' element={<Friends />} />
-        </Route>
-
-        <Route exact path='/quickPlay' element={<ProtectedRoute />}>
-          <Route exact path='/quickPlay' element={<QuickPlay />} />
         </Route>
 
         <Route exact path='/leagues' element={<ProtectedRoute />}>
           <Route exact path='/leagues' element={<Leagues />} />
         </Route>
 
+        <Route exact path='/quickPlay' element={<ProtectedRoute />}>
+          <Route exact path='/quickPlay' element={<QuickPlay />} />
+        </Route>
+
         <Route exact path='/profile' element={<ProtectedRoute />}>
           <Route exact path='/profile' element={<Profile />} />
+          <Route exact path='/profile/edit' element={<ProfileEdit />} />
+        </Route>
+
+        <Route exact path='/search' element={<ProtectedRoute />}>
+          <Route exact path='/search' element={<SearchExplore />} />
+        </Route>
+
+        <Route exact path='/menu' element={<ProtectedRoute />}>
+          <Route exact path='/menu' element={<Menu />} />
+          <Route exact path='/menu/wallet' element={<Wallet />} />
+          <Route exact path='/menu/notification' element={<Notification />} />
+        </Route>
+
+        <Route exact path='/friends' element={<ProtectedRoute />}>
+          <Route exact path='/friends' element={<Friends />} />
         </Route>
 
         <Route path='/login' element={<Login />} />
