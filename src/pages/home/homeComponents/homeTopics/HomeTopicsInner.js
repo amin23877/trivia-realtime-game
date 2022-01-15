47f38b0df4dd01034d65e491d83852d43a5ef856
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-15 16:35:28 +0330

    {Add - HomeTopicsInner} /Add the basics template & styles /Add btn section

--- src/pages/home/homeComponents/homeTopics/HomeTopicsInner.js
+++ src/pages/home/homeComponents/homeTopics/HomeTopicsInner.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './HomeTopicsInner.scss';
 
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
+const handlePlay = () => {};
+
 const HomeTopicsInner = () => {
   return (
-    <div className='w-100 h-100 homeTopicsInner bg2'>HomeTopicsInner </div>
+    <div className='w-100 h-100 topicsInner'>
+      <div className='topicsInner-header'></div>
+
+      <div className='topicsInner-body'></div>
+
+      <div className='d-flex justify-content-center align-items-center topicsInner-footer'>
+        <button className='btn-play' onClick={handlePlay}>
+          <span className='mx-1'>Play</span>
+          <PlayArrowIcon className='mx-1' />
+        </button>
+      </div>
+    </div>
   );
 };
 export default HomeTopicsInner;
