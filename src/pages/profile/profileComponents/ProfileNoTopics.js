7b6d92acaecb6a9aef8ba6aa7a4f9b27da6fdf4d
amirmuha <mirzaii4046@gmail.com>
2022-01-19 17:33:20 +0330

    feat: added ProfileNoTopics component

---
+++ src/pages/profile/profileComponents/ProfileNoTopics.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+// --- assets
+import NoTopicsImg from "assets/images/pics/profile-notopics.svg"
+import "./ProfileNoTopics.scss"
+
+const ProfileNoTopics = () =>  {
+  return (
+    <div className="profile-no-topics">
+      <img src={NoTopicsImg}  alt="No topics image" />
+      <p className="profile-no-topics__text">
+        No Topics added to favorites
+      </p>
+    </div>
+  )
+}
+
+
+export default ProfileNoTopics;
+
+
