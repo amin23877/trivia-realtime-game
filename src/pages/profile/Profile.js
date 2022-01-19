931bd3536e64bc833a981e882b23f4e3047f74a5
amirmuha <mirzaii4046@gmail.com>
2022-01-19 16:36:16 +0330

    feat: added Profile page

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Layout from "../../common/components/layout/Layout"
+import ProfileHeader from "./profileComponents/ProfileHeader"
+import {P_TABS} from "./profileComponents/ProfileHeader"
+
 
 import './Profile.scss';
 
+
 const Profile = () => {
-  return <div className='w-100 h-100 profile bg1'>Profile</div>;
+  const [activeTab, setActiveTab] = useState(P_TABS.FAVORITE_TOPICS);
+  const tabHandler = (tab) => {
+    setActiveTab(tab)
+  }
+  return (
+    <Layout
+      header={false}
+      className="profile"
+    >
+      <ProfileHeader activeTab={activeTab} tabHandler={tabHandler} />
+      {/* <ProfileContents openTab={openTab} data={data} /> */}
+    </Layout>
+  );
 };
 export default Profile;
