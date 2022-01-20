812ec787e3ba308e026e69b4f45dbc2222276492
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-20 21:40:38 +0330

    {Add - Redux} /Handle modal's action

--- src/redux/actions/mainActions/generalActions.js
+++ src/redux/actions/mainActions/generalActions.js
@@ -5,3 +5,11 @@ import { actionsTypeGeneral } from './actionsType';
 export const SET_USER_INFO = (payload) => {
   return { type: actionsTypeGeneral.SET_USER_INFO, payload };
 };
+
+export const SET_MODALS = (payload) => {
+  // #modalRedux step2
+  return {
+    type: actionsTypeGeneral.SET_MODALS,
+    payload,
+  };
+};
--- src/redux/reducers/mainReducer/generalReducer.js
+++ src/redux/reducers/mainReducer/generalReducer.js
@@ -3,11 +3,14 @@ import { actionsTypeGeneral } from 'redux/actions/mainActions/actionsType';
 // #redux step4
 const initialState = {
   userInfo: {},
+  modals: {}, // #modalRedux step3
 };
 export const generalReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionsTypeGeneral.SET_USER_INFO:
       return { ...state, userInfo: action.payload };
+    case actionsTypeGeneral.SET_MODALS: // #modalRedux step4
+      return { ...state, modals: action.payload };
     default:
       return state;
   }
