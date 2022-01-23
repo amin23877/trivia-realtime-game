3b236852147532c8a39ea2a478f26f7cc5a96622
amirmuha <mirzaii4046@gmail.com>
2022-01-23 10:47:38 +0330

    fix: moved FriendCard.scss file into FriendCard.js

--- src/pages/profile/profileComponents/Contents/Friends/FriendCard.js
+++ src/pages/profile/profileComponents/Contents/Friends/FriendCard.js
@@ -1,21 +1,26 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+//----assets
+import "./FriendCard.scss"
+
 const FriendCard = ({data}) => {
   const removeFriendHandler = (id) => {
     console.log("Removing friend with id of "+id)
   }
   return (
-		<div className="profile-friends--item">
-			<div className="profile-friends--item__info">
-				<div className="profile-friends--item__info__avatar">
-					<img src={data.image} alt={data.name} />
+		<div className="profile-friend">
+			<div className="profile-friend--item">
+				<div className="profile-friend--item__info">
+					<div className="profile-friend--item__info__avatar">
+						<img src={data.image} alt={data.name} />
+					</div>
+					<p className="profile-friend--item__info__name">{data.name}</p>
 				</div>
-				<p className="profile-friends--item__info__name">{data.name}</p>
+				<button onClick={() => removeFriendHandler(data.id)} className="profile-friend--item__remove">
+					Remove
+				</button>
 			</div>
-			<button onClick={() => removeFriendHandler(data.id)} className="profile-friends--item__remove">
-				Remove
-			</button>
 		</div>
   );
 }
