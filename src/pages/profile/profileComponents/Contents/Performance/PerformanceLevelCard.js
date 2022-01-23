9e7d8f9ccdcea61a3e72636962d92a8c270e8b05
amirmuha <mirzaii4046@gmail.com>
2022-01-23 16:50:36 +0330

    update: updated progress bar

--- src/pages/profile/profileComponents/Contents/Performance/PerformanceLevelCard.js
+++ src/pages/profile/profileComponents/Contents/Performance/PerformanceLevelCard.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, {useRef,useEffect,useState} from "react"
 import PropTypes from "prop-types"
+import ProgressBar from "common/components/UI/ProgressBar"
 //----assets
 import "./PerformanceLevelCard.scss"
 import PerformanceLevelIcon from "assets/images/icons/performance-level-icon.svg"
@@ -12,8 +13,8 @@ export const PerformanceLevelCard = ({data}) => {
           XP: {data.xp}
         </div>
         <div className="level-card--xp__progress">
-          <progress value={data.xp} min={0} max={data.max_xp} />
-          <span>{data.max_xp}</span>
+          <ProgressBar value={data.xp} />
+          <span>{data.max_xp || 100}</span>
         </div>
       </div>
       <div className="level-card--level">
