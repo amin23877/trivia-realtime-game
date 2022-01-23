23d384f1aa30a979779f479a34a4c06bc6d35a08
amirmuha <mirzaii4046@gmail.com>
2022-01-23 16:39:30 +0330

    feat: added Search component

---
+++ src/common/components/UI/Search.js
@@ -0,0 +1,47 @@
+import React,{useState} from "react";
+import PropTypes from "prop-types";
+
+//---assets
+import "./Search.scss"
+import SearchIcon from "assets/images/icons/search-primary-icon.svg"
+
+const Search = ({
+  /**
+   * This callback will be called when user submit the form 
+   * @param value value entered by user into the search box will be passed to this callback as the first argument
+   * @example 
+   * ```js
+   * <Search cb={(value) => doSomething...} />
+   * ```
+  */
+  cb,
+  placeholder
+}) => {
+  const [value,setValue] = useState("");
+  const submitSearchHandler = (e) => {
+    e.preventDefault();
+    cb(value);
+    setValue("");
+  }
+  return (
+		<form onSubmit={submitSearchHandler} className="search--box">
+      <input
+        className="search--box__input"
+				value={value}
+				placeholder={placeholder}
+        onChange={e => setValue(e.currentTarget.value)}
+				autoComplete="off"
+			/>
+      <button className="search--box__button">
+        <img src={SearchIcon} alt="search icon" />
+      </button>
+		</form>
+  );
+}
+
+Search.propTypes = {
+  cb: PropTypes.func.isRequired,
+  placeholder: PropTypes.string
+}
+
+export default Search
