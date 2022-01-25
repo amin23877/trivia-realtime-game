b7578df4593c6faeed73930b79201cdb10772fac
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-25 21:23:56 +0330

    {Bug - App} /Fixed: Corect the base url

--- src/common/values/CORE.js
+++ src/common/values/CORE.js
@@ -1,12 +1,13 @@
-export const BASE_URL = 'http://quizupp.ir/api/';
-export const SOCKET_BASE_URL = 'http://quizup.site/';
-export const IMAGE_URL = 'http://quizup.site';
+// export const BASE_URL = 'http://quizupp.ir/api/';
+export const BASE_URL = "https://quizup.site/api/";
+export const SOCKET_BASE_URL = "http://quizup.site/";
+export const IMAGE_URL = "http://quizup.site";
 
 export const CORE = {
-  ERROR_MESSAGE: {
-    default: 'D Error',
-    server: 'Server Error',
-    timeOut: 'TimeOut Error',
-    notJson: 'NotJson Error',
-  },
+	ERROR_MESSAGE: {
+		default: "D Error",
+		server: "Server Error",
+		timeOut: "TimeOut Error",
+		notJson: "NotJson Error",
+	},
 };
