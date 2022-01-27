0c3515a1d65554f12c44e8b75379eadf3833e3d1
Asma Tanavar <a_tanavar@yahoo.com>
2022-01-27 18:48:14 +0330

    {Update - Layout} /Based on add WrapperLayoutFooter

--- src/common/components/layout/Layout.js
+++ src/common/components/layout/Layout.js
@@ -1,28 +1,28 @@
-import {React} from "react"
-import PropTypes from "prop-types"
-import Header from "../header/Header"
-import Footer from "../footer/Footer"
+import { React } from "react";
+import PropTypes from "prop-types";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
 
-const Layout = ({children,header = true,className = ""}) => {
-  return (
-    <div className={`w-100 h-100  ${className}`}>
-      {header &&
-        <div className="_header">
-          <Header />
-        </div>
-      }
-      {children}
-      <div style={{height: 70}} className="_footer w-100">
+const Layout = ({ children, header = true, className = "" }) => {
+	return (
+		<div className={`w-100 h-100  ${className}`}>
+			{header && (
+				<div className="_header">
+					<Header />
+				</div>
+			)}
+			{children}
+			{/* <div style={{height: 70}} className="_footer w-100">
         <Footer />
-      </div>
-    </div>
-  );
-}
+      </div> */}
+		</div>
+	);
+};
 
 Layout.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  header: PropTypes.bool,
-}
+	children: PropTypes.node,
+	className: PropTypes.string,
+	header: PropTypes.bool,
+};
 
-export default Layout
+export default Layout;
