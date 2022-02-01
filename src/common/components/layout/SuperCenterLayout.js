df1048fe97d2c3e226c3bb152c6a8c50e5247c6c
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-01 16:39:57 +0330

    add _bg-home class to layout

--- src/common/components/layout/SuperCenterLayout.js
+++ src/common/components/layout/SuperCenterLayout.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const SuperCenterLayout = () => {
 	return (
-		<div className="d-flex align-items-center justify-content-center w-100 h-100">
+		<div className="d-flex align-items-center justify-content-center w-100 h-100 _bg-home">
 			<Outlet />
 		</div>
 	);
