d6e0d8f7be517e0b12d2da58c84132494c6e4512
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-01 12:33:34 +0330

    implement details of sidebar

--- src/common/components/sidebar/Sidebar.js
+++ src/common/components/sidebar/Sidebar.js
@@ -4,7 +4,15 @@ import "./Sidebar.scss";
 
 // image
 import userImage from "assets/images/test/profile-pic-2.jpg";
-import { ReactComponent as HomeLogo } from "assets/images/icons/home.svg";
+import { ReactComponent as HomeIcon } from "assets/images/icons/home.svg";
+import { ReactComponent as LeagueIcon } from "assets/images/icons/league.svg";
+import { ReactComponent as UserIcon } from "assets/images/icons/user-icon.svg";
+import { ReactComponent as LeaderBoardIcon } from "assets/images/icons/leaderboard.svg";
+import { ReactComponent as SettingsIcon } from "assets/images/icons/settings-icon.svg";
+import { ReactComponent as ContactIcon } from "assets/images/icons/contact-us.svg";
+import { ReactComponent as LogoutIcon } from "assets/images/icons/logout.svg";
+import { ReactComponent as DeactivateIcon } from "assets/images/icons/deactivation.svg";
+import { ReactComponent as WalletIcon } from "assets/images/icons/wallet-icon.svg";
 
 const Sidebar = () => {
 	return (
@@ -19,41 +27,41 @@ const Sidebar = () => {
 
 			<div className="quick-play-btn">Quick Play</div>
 
-			<ul className="menu">
-				<li className="menu__item">
-					<HomeLogo />
+			<ul className="sidebar-menu">
+				<li className="sidebar-menu-item sidebar-menu-item_hover-effect-purple">
+					<HomeIcon />
 					Home
 				</li>
-				<li className="menu__item">
-					<HomeLogo />
+				<li className="sidebar-menu-item sidebar-menu-item_hover-effect-purple">
+					<LeagueIcon />
 					League
 				</li>
-				<li className="menu__item">
-					<HomeLogo />
+				<li className="sidebar-menu-item sidebar-menu-item_hover-effect-purple">
+					<UserIcon />
 					Profile
 				</li>
-				<li className="menu__item">
-					<HomeLogo />
+				<li className="sidebar-menu-item sidebar-menu-item_hover-effect-purple">
+					<LeaderBoardIcon />
 					Leaderboard
 				</li>
-				<li className="menu__item">
-					<HomeLogo />
+				<li className="sidebar-menu-item sidebar-menu-item_hover-effect-purple">
+					<WalletIcon />
 					Wallet
 				</li>
-				<li className="menu__item">
-					<HomeLogo />
+				<li className="sidebar-menu-item sidebar-menu-item_hover-effect-purple">
+					<SettingsIcon />
 					Settings
 				</li>
-				<li className="menu__item">
-					<HomeLogo />
+				<li className="sidebar-menu-item sidebar-menu-item_hover-effect-purple">
+					<ContactIcon />
 					Contact us
 				</li>
-				<li className="menu__item">
-					<HomeLogo />
+				<li className="sidebar-menu-item sidebar-menu-item_hover-effect-purple">
+					<LogoutIcon />
 					Logout
 				</li>
-				<li className="menu__item">
-					<HomeLogo />
+				<li className="sidebar-menu-item sidebar-menu-item_red sidebar-menu-item_hover-effect-red">
+					<DeactivateIcon />
 					deactivation
 				</li>
 			</ul>
