6d3528d6b4b9c6b043062672cd3b348eec6588ba
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-05 15:59:18 +0330

    using ExploreSearchBox component in desktop header

--- src/common/components/header/DesktopHeader.js
+++ src/common/components/header/DesktopHeader.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Search from "common/components/UI/Search";
+import { SearchExploreBox } from "pages/searchExplore/SearchExplore";
 import { useDispatch } from "react-redux";
+import { TOGGLE_NOTIF_DRAWER } from "redux/actions/mainActions/generalActions";
 
 import "./Header.scss";
 
 // images
 import desktopLogo from "assets/images/logo/logo-white.svg";
 import notifIcon from "assets/images/icons/notifications.svg";
-import { TOGGLE_NOTIF_DRAWER } from "redux/actions/mainActions/generalActions";
 
 const DesktopHeader = () => {
 	const dispatch = useDispatch();
@@ -20,7 +20,7 @@ const DesktopHeader = () => {
 		<div className="_header-shadow desktop-header">
 			<img src={desktopLogo} alt="logo" />
 
-			<Search cb={() => {}} />
+			<SearchExploreBox />
 
 			<img className="_cursor-pointer" src={notifIcon} alt="notifications" onClick={handleToggleDrawer} />
 		</div>
