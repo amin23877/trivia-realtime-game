02dea97ba346b7b2298145868ce9a74073c72e1d
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-05 15:52:54 +0330

    add user piece to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import thunk from "redux-thunk";
 import { topicsReducer } from "./reducers/topicReducer/topicsReducer";
 import { generalReducer } from "./reducers/mainReducer/generalReducer";
+import { userReducer } from "./reducers/userReducer/userReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
 	stateGeneral: generalReducer,
 	stateTopic: topicsReducer,
+	stateUser: userReducer,
 });
 
 const composeEnhancers = composeWithDevTools({});
