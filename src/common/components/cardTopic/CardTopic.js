60a50f87bda9358d2b8900070fc5b2b4ea55be5b
Asma Tanavar <a_tanavar@yahoo.com>
2022-02-07 09:46:02 +0330

    {Add - CardTopic } /Create

---
+++ src/common/components/cardTopic/CardTopic.js
@@ -0,0 +1,14 @@
+// Reacts
+import React from "react";
+// Hooks
+// Packages
+// Components, Services, Functions
+// Redux
+// Material - lab
+// Styles, Icons, Images
+import "./CardTopic.scss";
+
+const CardTopic = () => {
+	return <div className="w-100 h-100 cardTopic"></div>;
+};
+export default CardTopic;
