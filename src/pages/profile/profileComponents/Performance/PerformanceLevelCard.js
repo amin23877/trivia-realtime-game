c08145e7c53465790048375ac6e02a61b6715a1e
Asma Tanavar <a_tanavar@yahoo.com>
2022-02-08 00:42:36 +0330

    {Bug - PerformanceLevelCard} /Fix

--- src/pages/profile/profileComponents/Performance/PerformanceLevelCard.js
+++ src/pages/profile/profileComponents/Performance/PerformanceLevelCard.js
@@ -9,15 +9,15 @@ export const PerformanceLevelCard = ({ data }) => {
 	return (
 		<div className="level-card">
 			<div className="level-card--xp">
-				<div className="level-card--xp__value">XP: {data.xp}</div>
+				<div className="level-card--xp__value">XP: {data?.xp}</div>
 				<div className="level-card--xp__progress">
-					<ProgressBar value={data.xp} />
-					<span>{data.max_xp || 100}</span>
+					<ProgressBar value={data?.xp} />
+					<span>{data?.max_xp || 100}</span>
 				</div>
 			</div>
 			<div className="level-card--level">
 				<img src={PerformanceLevelIcon} alt="performance level icon" />
-				<span>{data.level}</span>
+				<span>{data?.level}</span>
 			</div>
 		</div>
 	);
