14cf14998a33b7849ac14677a2ac52287354c2d3
Asma Tanavar <a_tanavar@yahoo.com>
2022-02-07 15:57:00 +0330

    {Add - CardTopic} /Add template & Styles

--- src/common/components/cardTopic/CardTopic.js
+++ src/common/components/cardTopic/CardTopic.js
@@ -1,4 +1,5 @@
 // Reacts
+import { IMAGE_URL } from "common/values/CORE";
 import React from "react";
 // Hooks
 // Packages
@@ -7,8 +8,25 @@ import React from "react";
 // Material - lab
 // Styles, Icons, Images
 import "./CardTopic.scss";
+import iconRate from "assets/images/icons/rate-mini.svg";
 
-const CardTopic = () => {
-	return <div className="w-100 h-100 cardTopic"></div>;
+const CardTopic = ({ data, onClickCardTopic }) => {
+	return (
+		<div className="_wh-100 cardTopic">
+			<div className="_wh-100 card-img">
+				<img src={`${IMAGE_URL}${data?.logo}`} alt="" />
+			</div>
+			<div className="d-flex flex-column justify-content-between card-info">
+				<p className="title">{data?.name}</p>
+				<div className="d-flex justify-content-between align-items-center">
+					<p className="subtitle">{`${data?.singlePlays + data?.doublePlays} plays`}</p>
+					<p className="rate">
+						<img className="mx-1" src={iconRate} alt="" />
+						<span>{data?.rate}</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default CardTopic;
