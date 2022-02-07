eb6df6e24bd6fa77bdafab60fbab6c1e5d279971
Asma Tanavar <a_tanavar@yahoo.com>
2022-02-07 09:42:21 +0330

    {Update - EmptyList} /Dynamicize by type

--- src/common/components/empties/EmptyList.js
+++ src/common/components/empties/EmptyList.js
@@ -2,11 +2,21 @@ import React from "react";
 
 import "./Empties.scss";
 import imgEmptyList from "assets/images/pics/empty-list.png";
+import imgEmptyListProfileTopic from "assets/images/pics/empty-list-profile-topic.svg";
 
-const EmptyList = () => {
+const EmptyList = ({ type }) => {
 	return (
-		<div className="w-100 h-100 d-flex justify-content-center align-items-center">
-			<img className="_empty-list" src={imgEmptyList} alt="" />
+		<div className="_wh-100 d-flex justify-content-center align-items-center emptyList">
+			{type === "PROFILE_TOPIC" ? (
+				<div className="_wh-100 profile-topics">
+					<img src={imgEmptyListProfileTopic} alt="" />
+					<p className="message">No Topics added to favorites</p>
+				</div>
+			) : (
+				<>
+					<img className="_empty-list" src={imgEmptyList} alt="" />
+				</>
+			)}
 		</div>
 	);
 };
