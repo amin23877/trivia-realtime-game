fd0dc3be1850bfcb3a67fd1ea4dbc0066ef81b6f
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-12 15:30:38 +0330

    change home bg and add some class to super-centered layout

--- src/common/components/layout/SuperCenterLayout.js
+++ src/common/components/layout/SuperCenterLayout.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const SuperCenterLayout = () => {
 	return (
-		<div className="d-flex align-items-center justify-content-center w-100 h-100 _bg-home">
+		<div className="p-4 d-flex align-items-center justify-content-center w-100 h-100 overflow-auto _bg-login">
 			<Outlet />
 		</div>
 	);
