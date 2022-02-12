ad3f49759083243c7199eab826ce49fa15872757
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-12 16:54:35 +0330

    change padding breakpoint

--- src/common/components/layout/SuperCenterLayout.js
+++ src/common/components/layout/SuperCenterLayout.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const SuperCenterLayout = () => {
 	return (
-		<div className="p-4 d-flex align-items-center justify-content-center w-100 h-100 overflow-auto _bg-login">
+		<div className="p-xl-4 d-flex align-items-center justify-content-center w-100 h-100 overflow-auto _bg-login">
 			<Outlet />
 		</div>
 	);
