8a379cbd171efb7af14a1977bb8949c3a7114449
ali izadi <ali.izadi@chista.ir>
2022-02-16 12:19:57 +0330

    bugfix

---
+++ src/common/context/socket.js
@@ -0,0 +1,9 @@
+// import socketio from "socket.io-client";
+// import { SOCKET_BASE_URL } from "common/values/CORE";
+// import React from "react";
+// const token = localStorage.getItem("token") ? `${localStorage.getItem("token").replace('Bearer ', '')}` : null;
+
+// document.cookie = "token="+token;
+
+// export const socket = socketio.connect(SOCKET_BASE_URL, { transports: ["websocket"] });
+// export const SocketContext = React.createContext();
