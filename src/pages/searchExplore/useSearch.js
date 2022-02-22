7a20a89bd0c0cb3a16ba1f7be997606d790604e9
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-22 19:19:20 +0330

    create hook for searching

---
+++ src/pages/searchExplore/useSearch.js
@@ -0,0 +1,24 @@
+import { useDebounce } from "common/hooks/useDebounce";
+import { useEffect, useState } from "react";
+import { useRequest } from "common/hooks/useRequest";
+
+/*
+ * 	this hook get search query (input value) and return search result with debounce
+ * */
+export const useSearch = (query) => {
+	const debouncedQuery = useDebounce(query, 1000);
+
+	const [totalResult, setTotalResult] = useState(null);
+
+	// search on users and topics
+	const { response: topics, success: fetchTopicsSuccess } = useRequest(`/topic?containstag=${debouncedQuery}`);
+	const { response: users, success: fetchUsersSuccess } = useRequest(`/user?containsname=${debouncedQuery}`);
+
+	useEffect(() => {
+		if (fetchUsersSuccess && fetchTopicsSuccess) {
+			setTotalResult([...topics.result, ...users.result]);
+		}
+	}, [fetchTopicsSuccess, fetchUsersSuccess, topics, users]);
+
+	return totalResult;
+};
