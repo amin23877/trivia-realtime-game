6023e9061ffbcfa0676232927d8e28c5c1604e22
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-23 21:41:22 +0330

    link to friend profile and update data properties

--- src/pages/profile/profileTabs/Friends/FriendCard.js
+++ src/pages/profile/profileTabs/Friends/FriendCard.js
@@ -16,17 +16,18 @@ const FriendCard = ({ data, index }) => {
 		dispatch(SET_MODALS({ [MODALS.generalModal]: true }));
 	};
 
-	const removeFriendHandler = (id) => {
-		console.log("Removing friend with id of " + id);
+	const removeFriendHandler = () => {
+		// remove
 	};
 
 	return (
 		<>
 			<ListItem
+				index={index + 1}
 				className="profile-friend__list-item"
-				key={index}
-				avatar={data.image}
-				username={data.name}
+				link={"/profile/" + data._id}
+				avatar={data.avatar}
+				username={data.username}
 				avatarSize={{ mobile: 38, desktop: 50 }}
 			>
 				<span onClick={handleOpen} className="profile-friend__remove">
@@ -37,9 +38,9 @@ const FriendCard = ({ data, index }) => {
 			{stateGeneral.modals[MODALS.generalModal] ? (
 				<GeneralModal
 					actionText="Remove"
-					question={`Remove ${data?.name} from Friends ?`}
-					icon={data.image}
-					cb={() => removeFriendHandler(data?.id || "No id provided to remove a friend.")}
+					question={`Remove ${data?.username} from Friends ?`}
+					icon={data.avatar}
+					cb={() => removeFriendHandler()}
 				/>
 			) : null}
 		</>
