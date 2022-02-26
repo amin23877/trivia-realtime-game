b92897d14033cc6b342353b3475bbff2f989376e
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-26 20:45:27 +0330

    update ChangeAvatar with new modal implementation

--- src/pages/profile-edit/profileEditComponents/ChangeAvatar.js
+++ src/pages/profile-edit/profileEditComponents/ChangeAvatar.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import Avatar from "common/components/UI/Avatar";
-import ChangeProfilePicModal from "pages/modals/ChangeProfilePicModal";
+import ChangeProfilePicModal from "common/components/modals/ChangeProfilePicModal";
 
 import "./ChangeAvatar.scss";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,11 +14,6 @@ const ChangeAvatar = () => {
 	const dispatch = useDispatch();
 	const avatar = useSelector((state) => state.stateUser.userInfo.avatar);
 
-	const [modalOpen, setModalOpen] = useState(false);
-
-	const handleModalOpen = () => setModalOpen(true);
-	const handleModalClose = () => setModalOpen(false);
-
 	const removeAvatar = () => {
 		// 1 - remove avatar from state
 		// 2 - request to server for remove avatar
@@ -26,22 +21,22 @@ const ChangeAvatar = () => {
 
 	const changeAvatar = (file) => {
 		dispatch(updateUser("avatar", file));
-
-		setModalOpen(false);
 	};
 
 	return (
 		<div className="ml-5">
-			<Avatar
-				onClick={handleModalOpen}
-				src={IMAGE_URL + encodeURI(avatar)}
-				size={{ mobile: 72, desktop: 100 }}
-				className="change-avatar-btn"
+			<ChangeProfilePicModal
+				onRemove={removeAvatar}
+				onChange={changeAvatar}
+				renderButton={(handleOpen) => (
+					<Avatar
+						onClick={handleOpen}
+						src={IMAGE_URL + encodeURI(avatar)}
+						size={{ mobile: 72, desktop: 100 }}
+						className="change-avatar-btn"
+					/>
+				)}
 			/>
-
-			{modalOpen && (
-				<ChangeProfilePicModal onRemove={removeAvatar} cb={changeAvatar} onClose={handleModalClose} />
-			)}
 		</div>
 	);
 };
