a88e3396c42637c77c9dd202c50935230b185c0c
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-02-28 22:19:43 +0330

    use Confirm modal for change profile image

--- src/pages/profile-edit/profileEditComponents/ChangeAvatar.js
+++ src/pages/profile-edit/profileEditComponents/ChangeAvatar.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import Avatar from "common/components/UI/Avatar";
-import ChangeProfilePicModal from "common/components/modals/ChangeProfilePicModal";
+import ConfirmModal from "common/components/modals/ConfirmModal";
 
 import "./ChangeAvatar.scss";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,25 +13,35 @@ import { ReactComponent as TrashIcon } from "assets/images/icons/delete-icon.svg
  * 	this component handle user avatar change
  * */
 const ChangeAvatar = () => {
+	const fileInputRef = useRef();
+
 	const dispatch = useDispatch();
 	const avatar = useSelector((state) => state.stateUser.userInfo.avatar);
 
-	const removeAvatar = () => {
+	const selectFileHandler = () => fileInputRef?.current?.click();
+
+	const handleRemoveAvatar = () => {
 		// 1 - remove avatar from state
 		// 2 - request to server for remove avatar
 	};
 
-	const changeAvatar = (file) => {
-		dispatch(updateUser("avatar", file));
+	const handleChangeAvatar = (e) => {
+		const file = e.currentTarget.files[0];
+
+		if (file) dispatch(updateUser("avatar", file));
 	};
 
 	return (
 		<div className="change-avatar-container">
+			<input ref={fileInputRef} type="file" onChange={handleChangeAvatar} hidden />
+
 			<Avatar src={IMAGE_URL + encodeURI(avatar)} size={{ mobile: 72, desktop: 156 }} />
 
-			<ChangeProfilePicModal
-				onRemove={removeAvatar}
-				onChange={changeAvatar}
+			<ConfirmModal
+				alertIcon={false}
+				question="Change Profile Photo?"
+				actionText="Select Photo"
+				action={selectFileHandler}
 				renderButton={(handleOpen) => (
 					<span onClick={handleOpen} className="change-avatar-btn">
 						<EditIcon />
@@ -39,9 +49,16 @@ const ChangeAvatar = () => {
 				)}
 			/>
 
-			<span className="change-avatar-btn">
-				<TrashIcon />
-			</span>
+			<ConfirmModal
+				alertIcon={false}
+				question="Remove Profile Photo?"
+				actionText="Remove"
+				renderButton={(handleOpen) => (
+					<span onClick={handleOpen} className="change-avatar-btn">
+						<TrashIcon />
+					</span>
+				)}
+			/>
 		</div>
 	);
 };
