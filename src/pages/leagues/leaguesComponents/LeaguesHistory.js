278a31e7cc1e10d007fea186bf1546a1387b1e9c
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-02 23:21:14 +0330

    use useRequest hook for data fetching

--- src/pages/leagues/leaguesComponents/LeaguesHistory.js
+++ src/pages/leagues/leaguesComponents/LeaguesHistory.js
@@ -1,81 +1,49 @@
 // Reacts
-import React, { useEffect, useState } from "react";
+import React, { useMemo } from "react";
 // Hooks
 import { useNavigate } from "react-router-dom";
 // Packages
 // Components, Services, Functions
-import ApiCall from "common/services/ApiCall";
-import { MOCK_CARDINFO } from "common/mocks/MOCK";
 import EmptyList from "common/components/empties/EmptyList";
 import CardLeagueInfo from "common/components/cardLeagueInfo/CardLeagueInfo";
-// Redux
-import { useDispatch } from "react-redux";
-// Material - lab
+
 // Styles, Icons, Images
 import "./LeaguesHistory.scss";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import { SET_SPINNER } from "redux/actions/mainActions/generalActions";
 import { TYPE_LEAGUE_HOME } from "common/values/TYPES";
+import { useRequest } from "common/hooks/useRequest";
 
 const LeaguesHistory = () => {
 	const navigate = useNavigate();
-	const apiCall = new ApiCall();
-	const dispatch = useDispatch();
 
-	const currentDate = Date.now();
-
-	const [dataLeagueHistory, setDataLeagueHistory] = useState([]);
-
-	const mockHistory = [MOCK_CARDINFO, MOCK_CARDINFO, MOCK_CARDINFO, MOCK_CARDINFO, MOCK_CARDINFO, MOCK_CARDINFO];
+	const currentDate = useMemo(() => Date.now(), []);
+	const { response: dataLeagueHistory, status } = useRequest(`league?minendtime=${currentDate}`);
 
 	const handleGoBack = () => {
 		navigate(-1);
 	};
 
-	const getDataLeagueHistory = () => {
-		dispatch(SET_SPINNER(true));
-		apiCall
-			.get(`league?minendtime=${currentDate}`)
-			.then((res) => {
-				dispatch(SET_SPINNER(false));
-				// console.log(res.data);
-				setDataLeagueHistory(res.data);
-			})
-			.catch((err) => {
-				dispatch(SET_SPINNER(false));
-				// console.log(err);
-			});
-	};
-
-	useEffect(() => {
-		let isMounted = true;
-		if (isMounted) {
-			getDataLeagueHistory();
-		}
-		return () => {
-			isMounted = false;
-		};
-	}, []);
-
 	return (
-		<div className="w-100 h-100 history">
-			<div className="history-header">
-				<ArrowBackIcon className="color-primary" onClick={handleGoBack} />
-				<div className="_header-title color-primary mx-2">League History</div>
-			</div>
+		status === "success" && (
+			<div className="w-100 h-100 history">
+				<div className="history-header">
+					<ArrowBackIcon className="color-primary" onClick={handleGoBack} />
+					<div className="_header-title color-primary mx-2">League History</div>
+				</div>
 
-			<div className="history-body ">
-				{dataLeagueHistory.length > 0 ? (
-					dataLeagueHistory?.map((el, index) => (
-						<div key={index} className="ratio mb-3 mb-xl-3 _dish-cardLeagueInfo">
-							<CardLeagueInfo info={el} type={TYPE_LEAGUE_HOME.HISTORY} />
-						</div>
-					))
-				) : (
-					<EmptyList />
-				)}
+				<div className="history-body ">
+					{dataLeagueHistory ? (
+						dataLeagueHistory?.map((el, index) => (
+							<div key={index} className="ratio mb-3 mb-xl-3 _dish-cardLeagueInfo">
+								<CardLeagueInfo info={el} type={TYPE_LEAGUE_HOME.HISTORY} />
+							</div>
+						))
+					) : (
+						<EmptyList />
+					)}
+				</div>
 			</div>
-		</div>
+		)
 	);
 };
 export default LeaguesHistory;
