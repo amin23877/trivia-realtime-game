4c90867cd4f8198fff584c7dcd6bfc0dbd6850b3
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-03 20:52:39 +0330

    change list item style for current user

--- src/common/components/UI/list/List.js
+++ src/common/components/UI/list/List.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import s from "./List.module.scss";
 import CardUser from "common/components/cardUser/CardUser";
+import { useSelector } from "react-redux";
 
 export const List = ({ children, className }) => {
 	return <div className={`${s.list} ${className}`}>{children}</div>;
@@ -20,10 +21,12 @@ export const ListFooter = ({ children, ...rest }) => {
 };
 
 export const ListItem = ({ children, className, userId, index, avatar, username = "", info }) => {
+	const id = useSelector((state) => state.stateUser.userInfo._id);
+
 	return (
-		<div className={`${s.listItemContainer} ${className}`}>
+		<div className={`${s.listItemContainer} ${className} ${id === userId ? s.currentUser : ""}`}>
 			<div className={s.listItemInfo}>
-				{index && <span className={s.index}>{index}.</span>}
+				{index && <span className={`${s.index} ${id === userId ? s.currentUserIndex : ""}`}>{index}.</span>}
 
 				<CardUser id={userId} username={username} info={info} avatar={avatar} />
 			</div>
