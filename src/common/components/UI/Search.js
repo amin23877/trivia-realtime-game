88f48a2b9819cbd8aad669cdced4561a8f01829d
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-03 12:45:51 +0330

    handle profile firends search

--- src/common/components/UI/Search.js
+++ src/common/components/UI/Search.js
@@ -1,45 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 //---assets
 import "./Search.scss";
 
-const Search = ({
-	/**
-	 * This callback will be called when user submit the form
-	 * @param value value entered by user into the search box will be passed to this callback as the first argument
-	 * @example
-	 * ```js
-	 * <Search cb={(value) => doSomething...} />
-	 * ```
-	 */
-	cb,
-	placeholder,
-}) => {
-	const [value, setValue] = useState("");
-	const submitSearchHandler = (e) => {
-		e.preventDefault();
-		cb(value);
-		setValue("");
-	};
+const Search = ({ value, onChange, placeholder }) => {
 	return (
-		<form onSubmit={submitSearchHandler} className="search--box">
+		<div className="search--box">
 			<input
 				className="search--box__input"
 				value={value}
 				placeholder={placeholder}
-				onChange={(e) => setValue(e.currentTarget.value)}
+				onChange={onChange}
 				autoComplete="off"
 			/>
-			<button className="search--box__button">
-				<div className="search--box__icon" />
-			</button>
-		</form>
+
+			<div className="search--box__icon" />
+		</div>
 	);
 };
 
 Search.propTypes = {
-	cb: PropTypes.func.isRequired,
+	value: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
 	placeholder: PropTypes.string,
 };
 
--- src/pages/profile/profileTabs/Friends/ProfileFriends.js
+++ src/pages/profile/profileTabs/Friends/ProfileFriends.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRequest } from "common/hooks/useRequest";
+import { useDebounce } from "common/hooks/useDebounce";
 
 //----assets
 import "./ProfileFriends.scss";
@@ -10,22 +11,28 @@ import EmptyFriendsList from "common/components/empties/EmptyFriendsList";
 import FriendCard from "pages/profile/profileTabs/Friends/FriendCard";
 
 const ProfileFriends = () => {
-	const { response: friends, success } = useRequest(`/user/me/friends`);
+	const [searchQuery, setSearchQuery] = useState("");
 
-	if (success && !friends.length) return <EmptyFriendsList />;
+	const debouncedQuery = useDebounce(searchQuery, 1000);
+
+	const { response: friends, status } = useRequest(`/user/me/friends?username=${debouncedQuery}`);
+
+	const handleSearch = (e) => setSearchQuery(e.target.value);
+
+	if (status === "success" && !friends.length && !debouncedQuery) return <EmptyFriendsList />;
 
 	return (
-		success && (
-			<div className="profile-friend">
-				<Search cb={(value) => console.log(value)} />
+		<div className="profile-friend">
+			<Search value={searchQuery} onChange={handleSearch} />
 
+			{status === "success" && (
 				<div className="profile-friend__list">
 					{friends?.map((el, index) => (
 						<FriendCard key={index} index={index} data={el} />
 					))}
 				</div>
-			</div>
-		)
+			)}
+		</div>
 	);
 };
 
