ef7be94ea39cafb81c9a7ee75995017b72070a9a
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-03 20:25:28 +0330

    remove card league from inner topic

--- src/pages/innerTopic/InnerTopic.js
+++ src/pages/innerTopic/InnerTopic.js
@@ -1,50 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import TopicCardInner from "pages/innerTopic/innerTopicComponents/TopicCardInner";
 import PlayFooter from "common/components/footer/PlayFooter";
-import CardLeagueInfo from "common/components/cardLeagueInfo/CardLeagueInfo";
 import TopicLeaderboard from "pages/innerTopic/innerTopicComponents/TopicLeaderboard";
 import { useRequest } from "common/hooks/useRequest";
 import { useParams } from "react-router-dom";
-import ApiCall from "common/services/ApiCall";
 import { useDispatch } from "react-redux";
-import { SET_SPINNER, SET_TYPE_LEADERBOARD_COMPONENT } from "redux/actions/mainActions/generalActions";
+import { SET_TYPE_LEADERBOARD_COMPONENT } from "redux/actions/mainActions/generalActions";
 import { TYPE_LEADERBOARD_COMPONENT } from "common/values/TYPES";
 
 import s from "./InnerTopic.module.scss";
-
-const api = new ApiCall();
 
 const InnerTopic = () => {
 	let { id } = useParams();
 	const dispatch = useDispatch();
 
-	const [dataLeague, setDataLeague] = useState();
-
 	const { response: dataInnerTopic, success } = useRequest(`topic/${id}`);
 
-	const getDataLeague = () => {
-		dispatch(SET_SPINNER(true));
-		api.get(`league?TopicId=${id}&minendTime=${Date.now()}`)
-			.then((res) => {
-				dispatch(SET_SPINNER(false));
-				res.data.length > 0 ? setDataLeague(res.data[0]) : setDataLeague({});
-			})
-			.catch(() => {
-				dispatch(SET_SPINNER(false));
-			});
-	};
-
 	useEffect(() => {
-		let isMounted = true;
-		if (isMounted) {
-			dispatch(SET_TYPE_LEADERBOARD_COMPONENT(TYPE_LEADERBOARD_COMPONENT.INNER_TOPIC));
-			// getDataInnerTopic();
-			getDataLeague();
-		}
-		return () => {
-			isMounted = false;
-		};
-	}, []);
+		dispatch(SET_TYPE_LEADERBOARD_COMPONENT(TYPE_LEADERBOARD_COMPONENT.INNER_TOPIC));
+	}, [dispatch]);
 
 	return (
 		success && (
@@ -52,12 +26,6 @@ const InnerTopic = () => {
 				<TopicCardInner id={id} data={dataInnerTopic} />
 
 				<div className={s.contentSection}>
-					{dataLeague && (
-						<div className="ratio _dish-cardLeagueInfo">
-							<CardLeagueInfo info={dataLeague} />
-						</div>
-					)}
-
 					<p className={s.title}>Description</p>
 
 					<p className={s.text}>{dataInnerTopic?.description}</p>
