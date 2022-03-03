c044ccf36f88ffaeca528e7c92002f9c35229e7e
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-03 14:36:41 +0330

    create component for show user info and link to profile

---
+++ src/common/components/cardUser/CardUser.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Avatar from "common/components/UI/Avatar";
+import { IMAGE_URL } from "common/values/CORE";
+
+import s from "./CardUser.module.scss";
+
+const CardUser = ({ id, avatar, info, avatarSize = { mobile: 22, desktop: 46 }, username, classes = {} }) => {
+	const userId = useSelector((state) => state.stateUser.userInfo._id);
+
+	return (
+		<Link className={classes.container ?? s.container} to={`/profile/${id !== userId ? id : ""}`}>
+			<Avatar src={IMAGE_URL + encodeURI(avatar)} size={avatarSize} />
+
+			<div className={classes.content ?? s.content}>
+				<p className={classes.username ?? false}>{username}</p>
+				<p className={classes.info ?? s.info}>{info}</p>
+			</div>
+		</Link>
+	);
+};
+
+CardUser.propTypes = {
+	id: PropTypes.number.isRequired,
+	avatar: PropTypes.string,
+	info: PropTypes.string,
+	username: PropTypes.string,
+	avatarSize: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+	classes: PropTypes.object,
+};
+
+export default CardUser;
