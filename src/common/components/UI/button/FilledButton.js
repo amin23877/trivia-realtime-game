02931f49f7bac0d7c95b493244cc84434ad2accb
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-26 23:30:21 +0430

    render filled-button text with text component

--- src/common/components/UI/button/FilledButton.js
+++ src/common/components/UI/button/FilledButton.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import s from "./Button.module.scss";
+import Text from "common/components/UI/text/Text";
 
-const FilledButton = ({ children, as: Component = "div", className, variant = "primary", ...rest }) => {
+const FilledButton = ({ ns, as: Component = "div", className, variant = "primary", ...rest }) => {
 	const buttonStyle = variant === "primary" ? s.filledPrimary : s.filledSecondary;
 
 	return (
 		<Component {...rest} className={`${buttonStyle} ${className}`}>
-			{children}
+			<Text ns={ns} />
 		</Component>
 	);
 };
