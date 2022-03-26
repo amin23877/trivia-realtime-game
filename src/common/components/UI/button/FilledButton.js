47b845d8757a340b05ad16153e3ce509add6f9b7
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-27 01:12:44 +0430

    add props for icons

--- src/common/components/UI/button/FilledButton.js
+++ src/common/components/UI/button/FilledButton.js
@@ -3,12 +3,16 @@ import React from "react";
 import s from "./Button.module.scss";
 import Text from "common/components/UI/text/Text";
 
-const FilledButton = ({ ns, as: Component = "div", className, variant = "primary", ...rest }) => {
+const FilledButton = ({ ns, startIcon, endIcon, as: Component = "div", className, variant = "primary", ...rest }) => {
 	const buttonStyle = variant === "primary" ? s.filledPrimary : s.filledSecondary;
 
 	return (
 		<Component {...rest} className={`${buttonStyle} ${className}`}>
+			{startIcon && <div className={startIcon} />}
+
 			<Text ns={ns} />
+
+			{endIcon && <div className={endIcon} />}
 		</Component>
 	);
 };
