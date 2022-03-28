4fbc53f15d56c03167c65ca7e62744862e19df5c
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-29 00:08:09 +0430

    translate multi player game

--- src/pages/quickPlay/withFriends/EnterGameCode/EnterGameCode.js
+++ src/pages/quickPlay/withFriends/EnterGameCode/EnterGameCode.js
@@ -2,32 +2,40 @@ import { Button } from "@material-ui/core";
 import arrowBack from "assets/images/icons/arrow-back.svg";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './EnterGameCode.scss'
-const EnterGameCode = ({handleOpenCategories , joinCode , setJoinCode , handleJoinWithCode}) => {
-    const navigate = useNavigate();
+import "./EnterGameCode.scss";
+import Text from "common/components/UI/text/Text";
+const EnterGameCode = ({ handleOpenCategories, joinCode, setJoinCode, handleJoinWithCode }) => {
+	const navigate = useNavigate();
 
-    return (
-        <div className="enter-game-code w-100 h-100">
-            <div className="enter-game-code__header">
-                <img onClick={() => navigate('/')} src={arrowBack} />
-                <p>Play With Friends</p>
-            </div>
-            <div className="enter-game-code__join-code">
-                <p>Enter a join code</p>
-                <div className="enter-game-code__join-code--form">
-                    <input value={joinCode} onChange={(e) => setJoinCode(e.target.value)} />
-                    <Button onClick={handleJoinWithCode} className={`${joinCode==''?'enter-game-code__join-code--form--disabled':''}`} disabled={joinCode == ''}>Join</Button>
-                </div>
-            </div>
-            <div className="enter-game-code__create-game">
-                <p>Create a new game to play with friends</p>
-                <div className="enter-game-code__create-game--button">
-                <Button onClick={handleOpenCategories}>Create a Game</Button>
-
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="enter-game-code w-100 h-100">
+			<div className="enter-game-code__header">
+				<img onClick={() => navigate("/")} src={arrowBack} />
+				<Text ns="play-with-friend" />
+			</div>
+			<div className="enter-game-code__join-code">
+				<Text ns="enter-join-code" />
+				<div className="enter-game-code__join-code--form">
+					<input value={joinCode} onChange={(e) => setJoinCode(e.target.value)} />
+					<Button
+						onClick={handleJoinWithCode}
+						className={`${joinCode == "" ? "enter-game-code__join-code--form--disabled" : ""}`}
+						disabled={joinCode == ""}
+					>
+						<Text as="span" ns="join" />
+					</Button>
+				</div>
+			</div>
+			<div className="enter-game-code__create-game">
+				<Text ns="create-game-msg" />
+				<div className="enter-game-code__create-game--button">
+					<Button onClick={handleOpenCategories}>
+						<Text as="span" ns="create-game" />
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default EnterGameCode;
--- src/pages/quickPlay/withFriends/FriendsList/FriendsList.js
+++ src/pages/quickPlay/withFriends/FriendsList/FriendsList.js
@@ -4,63 +4,82 @@ import inviteIcon from "assets/images/icons/header-people.svg";
 import chainLinkIcon from "assets/images/icons/chainLink.svg";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './FriendsList.scss'
-import userIcon from 'assets/images/icons/footer-profile.svg';
+import "./FriendsList.scss";
+import userIcon from "assets/images/icons/footer-profile.svg";
 import { IMAGE_URL } from "common/values/CORE";
-import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
+import PlayArrowRoundedIcon from "@material-ui/icons/PlayArrowRounded";
+import Text from "common/components/UI/text/Text";
 
 const FriendsList = ({ joinCode, users, mpGamesId, handleLeaveGame, handleStartGame, type }) => {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const handleCopyLink = () => {
+	const handleCopyLink = () => {
+		/* Copy the text inside the text field */
+		navigator.clipboard.writeText("https://app.quizup.site/quickPlay/withFriends?code=" + joinCode);
 
-        /* Copy the text inside the text field */
-        navigator.clipboard.writeText('https://app.quizup.site/quickPlay/withFriends?code='+joinCode);
-
-        /* Alert the copied text */
-        alert("link copied to clipboard");
-    }
-    return (
-        <div className="friends-list w-100 h-100">
-            <div className="friends-list__header">
-                <p>Join Code:</p>
-                <p>{joinCode}</p>
-                <div className="friends-list__header--invite">
-                    <img src={inviteIcon} />
-                    <p>Invite</p>
-                </div>
-                <div onClick={handleCopyLink} className="friends-list__header--copy">
-                    <img src={chainLinkIcon} />
-                    <p>Copy Link</p>
-                </div>
-            </div>
-            <div className="friends-list__users-list-container">
-                <p>People who have joined</p>
-                <div className="friends-list__users-list-container--users-list">
-                    {users.map((user) => (
-                        <div className="friends-list__users-list-container--users-list--user">
-                            <div>
-                                <img src={user.avatar ? IMAGE_URL + user.avatar : userIcon} />
-                                <p>{user.username || user.name}</p>
-                            </div>
-                            <div>
-                                {joinCode == (type === 'quickPlay' ? mpGamesId.categoryGameId : mpGamesId.topicGameId) &&
-                                    <p>Remove</p>
-                                }
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className="friends-list__footer">
-                <Button onClick={handleLeaveGame}>{joinCode == (type === 'quickPlay' ? mpGamesId.categoryGameId : mpGamesId.topicGameId) ? 'Cancel' : 'Leave'}</Button>
-                <Button onClick={handleStartGame} className={joinCode !== (type === 'quickPlay' ? mpGamesId.categoryGameId : mpGamesId.topicGameId) && 'disabled-btn'}>
-                    {joinCode == (type === 'quickPlay' ? mpGamesId.categoryGameId : mpGamesId.topicGameId) ? 'Start Game' : 'Wait To Start '}
-                    <PlayArrowRoundedIcon />
-                </Button>
-            </div>
-        </div>
-    )
-}
+		/* Alert the copied text */
+		alert("link copied to clipboard");
+	};
+	return (
+		<div className="friends-list w-100 h-100">
+			<div className="friends-list__header">
+				<p>
+					<Text ns="join-code" as="span" />:
+				</p>
+				<p>{joinCode}</p>
+				<div className="friends-list__header--invite">
+					<img src={inviteIcon} />
+					<Text ns="invite" />
+				</div>
+				<div onClick={handleCopyLink} className="friends-list__header--copy">
+					<img src={chainLinkIcon} />
+					<Text ns="btn.copy-link" />
+				</div>
+			</div>
+			<div className="friends-list__users-list-container">
+				<Text ns="joined-friends" />
+				<div className="friends-list__users-list-container--users-list">
+					{users.map((user) => (
+						<div className="friends-list__users-list-container--users-list--user">
+							<div>
+								<img src={user.avatar ? IMAGE_URL + user.avatar : userIcon} />
+								<p>{user.username || user.name}</p>
+							</div>
+							<div>
+								{joinCode ==
+									(type === "quickPlay" ? mpGamesId.categoryGameId : mpGamesId.topicGameId) && (
+									<Text ns="remove" />
+								)}
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="friends-list__footer">
+				<Button onClick={handleLeaveGame}>
+					{joinCode == (type === "quickPlay" ? mpGamesId.categoryGameId : mpGamesId.topicGameId) ? (
+						<Text as="span" ns="cancel" />
+					) : (
+						<Text as="span" ns="leave" />
+					)}
+				</Button>
+				<Button
+					onClick={handleStartGame}
+					className={
+						joinCode !== (type === "quickPlay" ? mpGamesId.categoryGameId : mpGamesId.topicGameId) &&
+						"disabled-btn"
+					}
+				>
+					{joinCode == (type === "quickPlay" ? mpGamesId.categoryGameId : mpGamesId.topicGameId) ? (
+						<Text as="span" ns="start-game" />
+					) : (
+						<Text as="span" ns="wait-to-start" />
+					)}
+					<PlayArrowRoundedIcon />
+				</Button>
+			</div>
+		</div>
+	);
+};
 
 export default FriendsList;
