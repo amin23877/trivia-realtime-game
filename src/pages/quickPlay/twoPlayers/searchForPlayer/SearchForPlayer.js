d1062c15d6be4f532622f043c49a5f54a5de9ca5
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-28 23:37:24 +0430

    translate search playmate page

--- src/pages/quickPlay/twoPlayers/searchForPlayer/SearchForPlayer.js
+++ src/pages/quickPlay/twoPlayers/searchForPlayer/SearchForPlayer.js
@@ -1,19 +1,22 @@
-import './SearchForPlayer.scss'
-import searchForUserCenter from 'assets/images/icons/searchForUserCenter.svg';
-import searchForUserRing from 'assets/images/icons/searchForUserRing.svg';
-import { Button } from '@material-ui/core';
+import "./SearchForPlayer.scss";
+import searchForUserCenter from "assets/images/icons/searchForUserCenter.svg";
+import searchForUserRing from "assets/images/icons/searchForUserRing.svg";
+import { Button } from "@material-ui/core";
+import Text from "common/components/UI/text/Text";
 
 const SearchForPlayer = ({ handleClose }) => {
-    return (
-        <div className="search-for-player">
-            <p className="search-for-player__title">Search for rival players</p>
-            <div className="search-for-player__icon">
-                <img src={searchForUserRing} className="search-for-player__icon--ring" />
-                <img src={searchForUserCenter} className="search-for-player__icon--center" />
-            </div>
-            <Button onClick={handleClose} className="search-for-player__cancel-btn">Cancel</Button>
-        </div>
-    )
-}           
- 
-export default SearchForPlayer
+	return (
+		<div className="search-for-player">
+			<Text ns="find-playmate" className="search-for-player__title" />
+			<div className="search-for-player__icon">
+				<img src={searchForUserRing} className="search-for-player__icon--ring" />
+				<img src={searchForUserCenter} className="search-for-player__icon--center" />
+			</div>
+			<Button onClick={handleClose} className="search-for-player__cancel-btn">
+				<Text as="span" ns="cancel" />
+			</Button>
+		</div>
+	);
+};
+
+export default SearchForPlayer;
